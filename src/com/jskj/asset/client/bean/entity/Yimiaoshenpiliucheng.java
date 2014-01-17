3ace255a9b647fd924041005917bf5a3f82d28b2
yelor <huiqi@huiqi-PC>
2014-01-17 21:43:47 +0800

    Merge master into HEAD
    Conflicts:
    	nbproject/project.properties

---
+++ src/com/jskj/asset/client/bean/entity/Yimiaoshenpiliucheng.java
@@ -0,0 +1,63 @@
+package com.jskj.asset.client.bean.entity;
+
+public class Yimiaoshenpiliucheng {
+    private String danjuId;
+
+    private String processId;
+
+    private String checkId1;
+
+    private String checkId2;
+
+    private String checkId3;
+
+    private String checkId4;
+
+    public String getDanjuId() {
+        return danjuId;
+    }
+
+    public void setDanjuId(String danjuId) {
+        this.danjuId = danjuId == null ? null : danjuId.trim();
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(String processId) {
+        this.processId = processId == null ? null : processId.trim();
+    }
+
+    public String getCheckId1() {
+        return checkId1;
+    }
+
+    public void setCheckId1(String checkId1) {
+        this.checkId1 = checkId1 == null ? null : checkId1.trim();
+    }
+
+    public String getCheckId2() {
+        return checkId2;
+    }
+
+    public void setCheckId2(String checkId2) {
+        this.checkId2 = checkId2 == null ? null : checkId2.trim();
+    }
+
+    public String getCheckId3() {
+        return checkId3;
+    }
+
+    public void setCheckId3(String checkId3) {
+        this.checkId3 = checkId3 == null ? null : checkId3.trim();
+    }
+
+    public String getCheckId4() {
+        return checkId4;
+    }
+
+    public void setCheckId4(String checkId4) {
+        this.checkId4 = checkId4 == null ? null : checkId4.trim();
+    }
+}
