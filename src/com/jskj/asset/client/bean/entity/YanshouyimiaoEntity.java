d95b51ba1b5c1a17180f56f21cd26287b1a9d562
yelor <huiqi@huiqi-PC>
2014-01-22 09:45:53 +0800

    0122

---
+++ src/com/jskj/asset/client/bean/entity/YanshouyimiaoEntity.java
@@ -0,0 +1,27 @@
+/*
+ * 2014 Chengdu JunChen Technology
+ */
+
+package com.jskj.asset.client.bean.entity;
+
+
+/**
+ *
+ * @author huiqi
+ */
+public class YanshouyimiaoEntity extends YiMiaotb{
+//    private YiMiaotb yimiao;
+    
+    private Yimiaoyanshou_detail_tb yimiaoyanshou_detail;
+
+
+    public Yimiaoyanshou_detail_tb getYimiaoyanshou_detail() {
+        return yimiaoyanshou_detail;
+    }
+
+    public void setYimiaoyanshou_detail(Yimiaoyanshou_detail_tb yimiaoyanshou_detail) {
+        this.yimiaoyanshou_detail = yimiaoyanshou_detail;
+    }
+    
+    
+}
