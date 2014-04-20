e6507d5d2b7e07258be8f6ae806b58a788d4615b
yelor <huiqi@huiqi-PC>
2014-04-20 18:09:06 +0800

    修改库存部分

--- src/com/jskj/asset/client/panel/baobiao/kucun/ErleiYimiaoKucunPanel.java
+++ src/com/jskj/asset/client/panel/baobiao/kucun/ErleiYimiaoKucunPanel.java
@@ -56,7 +56,7 @@ public final class ErleiYimiaoKucunPanel extends BasePanel {
         count = 0;
         bindTable = new BindTableHelper<StockpiletbAll>(jTableStockpile, new ArrayList<StockpiletbAll>());
         bindTable.createTable(new String[][]{{"stockpileId", "库存编号"}, {"yimiao.yimiaoId", "疫苗编号"}, {"yimiao.yimiaoName", "疫苗名称"}, {"yimiao.yimiaoJixing", "剂型"},
-        {"yimiao.yimiaoGuige", "疫苗规格"}, {"yimiao.unitId", "单位"}, {"stockpileQuantity", "库存数量"}, {"stockpileQuantity", "成本均价"}, {"stockpileTotalprice", "库存金额"}});
+        {"yimiao.yimiaoGuige", "疫苗规格"}, {"yimiao.unitId", "单位"}, {"stockpileQuantity", "库存数量"}, {"stockpilePrice", "成本均价"}, {"stockpileTotalprice", "库存金额"}});
         bindTable.setColumnType(Integer.class, 1);
         bindTable.bind().setColumnWidth(new int[]{0, 100}, new int[]{1, 100}, new int[]{2, 100}, new int[]{3, 150}, new int[]{5, 150}, new int[]{6, 100}, new int[]{7, 100}, new int[]{8, 150}).setRowHeight(30);
 
