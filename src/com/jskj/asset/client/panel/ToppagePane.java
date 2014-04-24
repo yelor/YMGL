918827891c1b6ca80621b4e8873da13bdb299f9a
305027939 <305027939@HCA-F4MM1Q1.clients.ap.health.ge.com>
2014-04-24 17:32:05 +0800

    增加库存，有效期警告信息

--- src/com/jskj/asset/client/panel/ToppagePane.java
+++ src/com/jskj/asset/client/panel/ToppagePane.java
@@ -95,7 +95,7 @@ public class ToppagePane extends BasePanel {
         @Override
         public Object doBackgrounp() {
             try {
-                jEditorTopPage.setPage(Constants.HTTP + "/AssetsSys/service/toppage");
+                jEditorTopPage.setPage(Constants.HTTP + "/AssetsSys/service/toppage?client");
             } catch (IOException e) {
                 jEditorTopPage.setText(e.getMessage());
                 AssetMessage.ERRORSYS(e.getMessage());
